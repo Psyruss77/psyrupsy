54e4ae50bc5b96f0e570f92e3fed4d9d66751ecb
psyruss <32247561+Psyruss@users.noreply.github.com>
2020-01-22 23:11:48 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,53 @@
+/* globals exports */
+'use strict';
+
+exports.id = 'com.add0n.native_client';
+exports.version = '0.4.2';
+
+exports.ids = {
+  chrome: [
+    'hfckgfbhdacemicpjljhfbjmkiggeche', // Send to VLC (Chrome)
+    'hgadfjhoalnaidfdoilgeimnolhcodbc', // Send to VLC (Opera)
+    'jdemocppkpiaocccmmdjnpclehbajogd', // Send to MPC (Chrome)
+    'ngaohleapbfjleillajbginbiaokmncm', // Send to MPC (Opera)
+    'bnjifjcdaiecofdljgempabjecjdjpnm', // Send to MPlayer (Chrome)
+    'kalnbllmkdiidgakcakkoecaahkpakmj', // Send to MPlayer (Opera)
+    'nlbhcidbelfhbpglaaoljbfpfpdkpdcg', // Download with IDM (Chrome)
+    'kajaikkhnmegmfnlifeklklaienhdekb', // Download with IDM (Opera)
+    'jlodlegnpjplclncjkgolcmdhjmlokna', // Download with FDM (Chrome)
+    'iidhcgjgmjodnfbghbgpdnhiajbmgcjo', // Download with FDM (Opera)
+    'jfpmbokkdeapjommajdfmmheiiakdlgo', // Download with JDownloader (Chrome)
+    'jjbbcngfknmgdlekfofhaagmogeifbpc', // Download with JDownloader (Opera)
+    'hlbdmmifbcfpccdnoknhdfcifiglaihb', // Download with DAP (Chrome)
+    'ekeecmblpnobdaijmfkcfcnofopooipg', // Download with DAP (Opera)
+    'jplcfomghgpjlgglocndioeceojhoika', // Download with Wget (Chrome)
+    'cmddfbnhpokddncdalabdlmdckcdmboh', // Download with Wget (Opera)
+    'gecfhmlaldelfoeahjahajoooenmnlnd', // Download with FlashGet (Opera)
+    'eadfknilcdakacgehgfoedimdghaedgi', // Download with FlashGet (Opera)
+    'jakcapoibgmjnghonlnjaooepccllgic', // Download with aria2 (Chrome)
+    'eiejadjdcchfommckmikpcdnihljbpai', // Edit with Sublime Text (Chrome)
+    'odbhmaaphiednnbgfionolmpkdplpilc', // Edit with Sublime Text (Opera)
+    'gpmjpkmohjgaimfhebdjebpgdnpcadji', // Edit with Notepad++ (Chrome)
+    'hfgnckeamhbkolmfeaieemklhejdcmai', // Edit with Notepad++ (Opera)
+    'jlghbmcdkfddnihedhlnmggjbfjcppng', // Send to MPV (Chrome)
+    'dbghnkdojpkebjbddhhjfjgapoolnikh', // Send to MPV (Opera)
+    'nglfpmpeljnfooojieocihijkeokinno', // Edit with VIM (Chrome)
+    'ajcbbbkgmbinpbkllamhphpgpgdabnhf', // Edit with VIM (Opera)
+  ],
+  firefox: [
+    '{3e0ac434-26e0-4c03-b757-3078486800c3}', // Send to VLC
+    '{cee1ccee-5508-4927-a929-9a557e63bbc8}', // Send to MPC
+    '{ccad95df-add6-4d8a-aa5c-cdc384075bab}', // Send to MPV
+    '{c7636e52-8aba-4717-9652-01922ee61eb3}', // Send to MPlayer media player
+    '{d1646fcf-76ad-49c5-b8b2-e496e9b71189}', // Download with IDM
+    '{cfd8df21-e05f-46e9-8ea1-af5e5177d492}', // Download with DAP
+    '{1fb1ffdc-b95d-451e-be52-7303adf9a0d3}', // Download with FDM
+    '{533953f8-ffb6-421c-af1a-5a02a792ab51}', // Download with Wget
+    '{48f5395d-5c00-41cd-9a5e-fd2f8d9b74c2}', // Download with FlashGet
+    '{03e07985-30b0-4ae0-8b3e-0c7519b9bdf6}', // Download with JDownloader
+    '{708bb4c5-336d-4a30-9126-8bf5d773bb41}', // Download with aria2
+    '{5510b212-951a-439c-ae73-b1ebbc68055f}', // Edit with Sublime Text
+    '{f91bc9ee-ae11-4850-8e6b-ed4b0262ce3b}', // Edit with Notepad++
+    '{dc393f22-0d98-44d4-8a2c-9dd72208e9f7}', // Edit with VIM
+  ]
+};
